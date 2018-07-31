85bbaa9f2789e1391a639b1ff9390735f83f3964
Seth <sethdanner@gmail.com>
2018-07-30 18:30:15 -0600

    Added model.

--- JournalWithLocalPersistence/Model+ModelController/Entry.swift
+++ JournalWithLocalPersistence/Model+ModelController/Entry.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class Entry {
+    
+    var title: String
+    var bodyText: String
+    var timestamp: Date = Date()
+    
+    init(title: String, bodyText: String) {
+        self.title = title
+        self.bodyText = bodyText
+    }
+}
+
+extension Entry: Equatable {
+    
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.title == rhs.title && lhs.bodyText == rhs.bodyText && lhs.timestamp == rhs.timestamp
+    }
+}
